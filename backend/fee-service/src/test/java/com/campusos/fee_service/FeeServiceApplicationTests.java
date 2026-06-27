fa52e1248df079c6d37da979306bf384d2582f64
saidaragani <saidaragani@gmail.com>
2026-06-27 17:09:07 +0530

    Initial CampusOS microservices structure

---
+++ backend/fee-service/src/test/java/com/campusos/fee_service/FeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusos.fee_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
